2666c1ff592fa361f0db0cd3afda4551887765fc
Khairi <khairiazman90@gmail.com>
2024-09-12 05:56:23 +0800

    added directional light 2d to mask night with a tint. added a signal reference example under daynight_indicator to check current day night state if need to refer

--- cowgame/scripts/date_time.gd
+++ cowgame/scripts/date_time.gd
@@ -25,4 +25,11 @@ func increase_by_sec(delta_seconds:float) -> void:
 	minutes = minutes % 60
 	hours = hours % 24
 	
-	print_debug(str(days)+ ":"+ str(hours)+ ":" + str(minutes)+ ":" + str(seconds))
+	#print_debug(str(days)+ ":"+ str(hours)+ ":" + str(minutes)+ ":" + str(seconds))
+
+func diff_without_days(other_time: DateTime) -> int:
+	var diff_hours = hours - other_time.hours
+	var diff_minutes = minutes - other_time.minutes + diff_hours * 60
+	var diff_seconds = minutes - other_time.seconds + diff_minutes * 60
+	
+	return diff_seconds
