5114df650677667fb49d38df50a5757962b18143
AimandotH <aimanhazim.business@gmail.com>
2024-09-13 00:57:55 +0800

    Added Global Variable
    Added Global Variable

---
+++ cowgame/scripts/Global.gd
@@ -0,0 +1,21 @@
+extends Node
+
+var gold = 10000
+
+var items = {
+	0: {
+		"Name": "Apple",
+		"Desc": "This is an Apple",
+		"Cost": 10
+	},
+	1: {
+		"Name": "Banana",
+		"Desc": "This is an Apple",
+		"Cost": 10
+	},
+	2: {
+		"Name": "Cherry",
+		"Desc": "This is an Apple",
+		"Cost": 10
+	}
+}
