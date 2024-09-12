180b1ed50328bfc5327b4ed51b6623f9e7d150e5
AimandotH <aimanhazim.business@gmail.com>
2024-09-12 23:27:39 +0800

    this should fix it for now
    fix1

--- cowgame/scripts/daynight_indicator.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-#signals from  day_night node will trigger this function
-func currentGameState(currentState):
-	
-	if currentState == 0:
-		print_debug("It's day time!")
-	else :
-		print_debug("It's night time!")
